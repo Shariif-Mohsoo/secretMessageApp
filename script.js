90913147f87fe14b427a673114d133c5745719c0
Shariif-Rajpoot <mr8781256@gmail.com>
2024-09-12 02:28:57 -0700

    Secure message-sharing app with Base64 encryption and dynamic link generation.

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+const { hash } = window.location;
+const deCrypted = atob(hash.replace("#", " "));
+
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  const input = document.querySelector("input");
-  console.log(input.value);
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+  // console.log(encrypted);
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+  // console.log(linkInput);
 });
 
-// const heading = document.body.querySelector("h1");
-// heading.innerHTML = deCrypted;
-// let text = heading.innerText;
-// if (text.length <= 30) heading.classList.add("heading-1");
-// else heading.classList.remove("heading-1");
+if (deCrypted) {
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#message-show").classList.remove("hide");
+  const heading = document.body.querySelector("h1");
+  heading.innerHTML = deCrypted;
+  let text = heading.innerText;
+  if (text.length <= 20) heading.classList.add("heading-1");
+  else heading.classList.remove("heading-1");
+}
